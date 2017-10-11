9a9bf58f25fd9339082c3003c0fe1f7524952406
ruizhouliu <ruizhouliu@163.com>
2017-10-11 17:32:45 +0800

    fix bugs

--- app/Http/Controllers/CfController.php
+++ app/Http/Controllers/CfController.php
@@ -70,6 +70,22 @@ class CfController extends Controller
         ]);
 
         $pdata = request()->all();
+
+        switch ($pdata['is_fba']){
+            case 0:
+                if($pdata['final_price'] * get_rate($pdata['from_site']) < gconfig('fbm.low.price')){
+                    return error('商品金额过低，存在刷单风险，请选择其他商品');
+                }
+                break;
+            case 1:
+                if($pdata['final_price'] * get_rate($pdata['from_site']) < gconfig('fba.low.price')){
+                    return error('商品金额过低，存在刷单风险，请选择其他商品');
+                }
+                break;
+            default:
+                throw new MsgException();
+        }
+
         if ($pdata['delivery_type'] == 1) {
             $pdata['delivery_addr'] = '';
         }
