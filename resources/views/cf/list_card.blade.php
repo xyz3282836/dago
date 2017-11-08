b2040d7e0f2b994ac8ea3c3638729023d1bcd007
zhou <ruizhouliu@163.com>
2017-11-08 23:23:25 +0800

    fix bugs

--- resources/views/cf/list_card.blade.php
+++ resources/views/cf/list_card.blade.php
@@ -97,8 +97,8 @@
                                     @{{ one.shop_id }}
                                     <br>
                                     <span style="font-size:12px;color:#464c5b;">(@{{ one.is_ld == 1?'秒杀':one.fba_text }})</span>
-                                    <br v-if="one.prime == 1">
-                                    <span v-if="one.prime == 1"><img height="12" src="/img/prime.png" /></span>
+                                    <br v-if="one.is_prime == 1">
+                                    <span v-if="one.is_prime == 1"><img height="12" src="/img/prime.png" /></span>
                                 </td>
                                 <td><span class="flag-icon" :class="'flag-icon-'+one.flag"></span></td>
                                 <td v-text="one.delivery_type_text"></td>
