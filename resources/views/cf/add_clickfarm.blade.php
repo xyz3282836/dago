f571217cf4ba785bbff4a6d5839434ef5475cfdf
zhou <ruizhouliu@163.com>
2017-11-14 00:44:09 +0800

    fix bugs

--- resources/views/cf/add_clickfarm.blade.php
+++ resources/views/cf/add_clickfarm.blade.php
@@ -344,7 +344,12 @@
                     }else{
                         tmp += {{$s4}} * this.task_num;
                     }
-                    tmp += this.searchprice * this.task_num + {{$s5 + $s6}} * this.task_num;
+                    if(this.is_ld == 1){
+                        tmp += this.searchprice * this.task_num + {{$s5}} * this.task_num;
+                    }
+                    if(this.is_prime == 1){
+                        tmp += this.searchprice * this.task_num + {{$s6}} * this.task_num;
+                    }
                     return tmp;
                 },
                 gettrans() {
